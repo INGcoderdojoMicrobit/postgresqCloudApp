48282bf7a3d0e85295e51faef59ca21d5b74468e
pixel <maks_lib@wp.pl>
2023-02-19 12:31:20 +0100

    HOTFIX: .catch przy możliwych błędach w client.connect().then()
    Możecie następnym razem korzystać z lintera?

--- index.js
+++ index.js
@@ -5,28 +5,27 @@ const bodyParser = require("body-parser");
 require("dotenv").config();
 const pg = require("pg");
 const pc = require("@prisma/client");
-const winston = require('winston');
+const winston = require("winston");
 
 // Imports the Google Cloud client library for Winston
-const {LoggingWinston} = require('@google-cloud/logging-winston');
+const { LoggingWinston } = require("@google-cloud/logging-winston");
 
 const loggingWinston = new LoggingWinston();
 
 // Create a Winston logger that streams to Cloud Logging
 // Logs will be written to: "projects/YOUR_PROJECT_ID/logs/winston_log"
 const logger = winston.createLogger({
-  level: 'info',
+  level: "info",
   transports: [
     new winston.transports.Console(),
     // Add Cloud Logging
-    loggingWinston,
-  ],
+    loggingWinston
+  ]
 });
 
 // Writes some log entries
 //logger.error('warp nacelles offline');
-logger.info('Odpalam logowanie');
-
+logger.info("Odpalam logowanie");
 
 const client = new pg.Client({
   user: process.env.DATABASE_USER,
@@ -40,19 +39,25 @@ if (process.env.INSTANCE_CONNECTION_NAME) {
 }
 
 if (process.env.DATABASE_URL) {
-    logger.info("Taki url do bazy danych mam: "+`${process.env.DATABASE_URL}`);
-} else
-{   
-    logger.info("Nie mam zdefiniowanego URL!");
+  logger.info("Taki url do bazy danych mam: " + `${process.env.DATABASE_URL}`);
+} else {
+  logger.info("Nie mam zdefiniowanego URL!");
 }
-  
+
 logger.info("Podpinam sie do bazy: " + `${client.host}-->${client.database}/${client.user}:${client.port} chmurowo? ${client.socketPath}`);
 
-client.connect().then(() => {
-  logger.info("Podpinam sie do bazy");
-  console.log("Connected to database: " + `${client.host}-->${client.database}/${client.user}:${client.port} chmurowo? ${client.socketPath}`);
-  logger.info("Connected to database: " + `${client.host}-->${client.database}/${client.user}:${client.port} chmurowo? ${client.socketPath}`);
-});
+client
+  .connect()
+  .then(() => {
+    logger.info("Podpinam sie do bazy");
+    console.log(
+      "Connected to database: " + `${client.host}-->${client.database}/${client.user}:${client.port} chmurowo? ${client.socketPath}`
+    );
+    logger.info(
+      "Connected to database: " + `${client.host}-->${client.database}/${client.user}:${client.port} chmurowo? ${client.socketPath}`
+    );
+  })
+  .catch(console.error);
 
 app.use(bodyParser.json());
 app.use(
@@ -92,34 +97,34 @@ app.use(async (req, res, next) => {
   res.header("X-Powered-By", "NothingSpecialTbh");
   req.db = client;
   req.log = logger;
-  
+
   const tokenHeader = req.header.Authorization;
   const tokenQuery = req.query.token;
 
   logger.info("Szukam tokena w bazie...");
-  
+
   if (tokenHeader) {
-    logger.info("Szukam: "+ `${tokenHeader}`);
+    logger.info("Szukam: " + `${tokenHeader}`);
     const u = await client.query("select * from tokens where token = $1", [tokenHeader]);
     if (u.rows.length == 0) return res.status(401).send("Unauthorized");
 
     if (u.rows[0].expires_at < new Date()) return res.status(401).send("Unauthorized - use proper credentials");
 
     req.userid = u.rows[0].user_id;
-    logger.info("znaleziony: "+ `${u.rows[0].user_id}`);
+    logger.info("znaleziony: " + `${u.rows[0].user_id}`);
 
     return next();
   }
 
   if (!tokenQuery) return res.status(401).send("Unauthorized");
-  logger.info("Szukam: "+ `${tokenQuery}`);
+  logger.info("Szukam: " + `${tokenQuery}`);
   const u = await client.query("select * from tokens where token = $1", [tokenQuery]);
   if (u.rows.length == 0) return res.status(401).send("Unauthorized");
 
   if (u.rows[0].expires_at < new Date()) return res.status(401).send("Unauthorized - use proper credentials");
 
   req.userid = u.rows[0].user_id;
-  logger.info("znaleziony: "+ `${u.rows[0].user_id}`);
+  logger.info("znaleziony: " + `${u.rows[0].user_id}`);
 
   return next();
 });
